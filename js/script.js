c099d348fae8ced15cd1d9b81c3ea1b407d8ffc0
aleksandra <122663153+motspan-aleksandra@users.noreply.github.com>
2024-05-03 20:06:25 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 // Массив с вопросами, вариантами ответов и правильными ответами
 
+let quizContainer = document.querySelector('.quiz-container');
+let loginContainer = document.querySelector('.login-container');
+let resultsContainer = document.querySelector('.results-container');
 let questions = [
     {
         question: "Какой язык программиирования вы изучаете?",
@@ -15,21 +18,55 @@ let questions = [
         question: "Что такое CSS?",
         options: ["Каскадные таблицы стилей", "Язык программирования", "База данных", "Библиотека"],
         correctAnswer: "Каскадные таблицы стилей"
+    },
+    {
+        question: "Какой знак исползуется для получения остатка от деления?",
+        options: ["/", "//", "%", "**"],
+        correctAnswer: "%"
+    },
+    {
+        question: "Как вывести что-либо в консоль?",
+        options: ["std::cout <<", "print()", "Console.WriteLine()", "console.log()"],
+        correctAnswer: "console.log()"
+    },
+    {
+        question: "Какой тег используется для вставки иображений на страницу?",
+        options: ["<img>", "<image>", "<picture></picture>", "photo"],
+        correctAnswer: "<img>"
     }
 ]
 
-let currentQuestion = 0 //Текущий вопрос
-let correctAnswers = 0 // Количество правильных ответов
+let currentQuestion = 0; //Текущий вопрос
+let correctAnswers = 0; // Количество правильных ответов
 
 // Функция для отображения текущего вопроса и вариантов ответа
 
+function displayLogin() {
+    loginContainer.style.display = "flex";
+    quizContainer.style.display = "none";
+    resultsContainer.style.display = "none";
+
+
+    let begin = document.querySelector(".begin");
+
+    begin.addEventListener('click', (e) => {
+        displayQuestion();
+    })
+
+}
+
+let input = document.querySelector(".input__name");
+
 function displayQuestion() {
+    loginContainer.style.display = "none";
+    quizContainer.style.display = "flex";
+    resultsContainer.style.display = "none";
     let result = document.getElementById("result");
-    result.textContent = correctAnswers
-    let questionElement = document.getElementById("question"); // получить блок для размещения вопроса
+    //result.textContent = correctAnswers
+    let questionElement = document.querySelector(".question"); // получить блок для размещения вопроса
     // размещаем вопрос на странице
     questionElement.textContent = `${currentQuestion + 1}. ${questions[currentQuestion].question}`; // интерполяция - sth like f string
-    let optionsElement = document.getElementById("options"); // Получить блок для размещения кнопки
+    let optionsElement = document.querySelector(".options"); // Получить блок для размещения кнопки
     optionsElement.innerHTML = ""; // Очищаем содержимое блока optionsElement
 
     // Получить массив ответов
@@ -39,18 +76,160 @@ function displayQuestion() {
 
     optionsArray.forEach((option) => {
         let button = document.createElement('button');
-        optionsElement.append(button);
         button.classList.add('btn');
         button.textContent = option;
+        optionsElement.append(button);
     });
 
-    optionsElement.addEventListener((e) => {
-        let targetOption = e.target;
-        if (targetOption.textContent = question[currentQuestion].correctAnswer) {
-            correctAnswers += 1;
+
+    let wrongAnswers = document.querySelector(".result__wrong-answers");
+    optionsElement.addEventListener('click', (e) => {
+        e.preventDefault();
+        if (currentQuestion >= questions.length - 1) {
+            displayResults();
         }
-        currentQuestion += 1;
+        let targetOption = e.target;
+        console.log("to: ", targetOption);
+        console.log(currentQuestion);
+        console.log(correctAnswers);
+        console.log("chosen:", targetOption.textContent);
+        console.log("correct answer:", questions[currentQuestion].correctAnswer);
+        if (targetOption.textContent == questions[currentQuestion].correctAnswer) { // THE MISTAKE WAS HERE, BECAUSE OF = INSTEAD OF == !
+            correctAnswers += 1;
+            console.log("correct answers upd:", correctAnswers);
+
+        } else {
+            let wronganswer = document.createElement("p");
+            wronganswer.classList.add("wronganswer");
+            //wronganswer.textContent = questions[currentQuestion].correctAnswer;
+            console.log("index of wrong question:", questions.indexOf(questions[currentQuestion]) + 1);
+            wronganswer.textContent = (questions.indexOf(questions[currentQuestion]) + 1).toString() + ". " + questions[currentQuestion].question;
+            wrongAnswers.append(wronganswer);
+            console.log("wrong answers:", wrongAnswers);
+            console.log("wrong answer:", wronganswer);
+
+        }
+        console.log(targetOption.textContent);
+        console.log(questions[currentQuestion].correctAnswer);
+
+        currentQuestion += 1; // получить блок для размещения вопроса
+        console.log(questions[currentQuestion].question)
+        questionElement.textContent = `${currentQuestion + 1}. ${questions[currentQuestion].question}`; // интерполяция - sth like f string
+        optionsElement.innerHTML = "";
+
+
+        let optionsArray = questions[currentQuestion].options;
+
+        // Создать кнопки с вариантами ответов и привязать к ним функциб перехода к следующему вопросу
+
+        optionsArray.forEach((option) => {
+            let button = document.createElement('button');
+            button.classList.add('btn');
+            button.textContent = option;
+            optionsElement.append(button);
+            //result.textContent = `${correctAnswers}/${questions.length}`
+        });
     })
+
+
+
+    /*
+    parent.addEventListener('click', (e) => {
+        console.log(e);
+        console.log('click');
+        e.preventDefault();
+
+
+        let targetBtn = e.target;
+
+        console.log('button defined');
+
+        if (targetBtn.getAttribute('class') == "key") {
+            if (targetBtn.textContent == "Space") {
+                input.textContent += " ";
+            } else if (targetBtn.textContent == "Backspace") {
+                console.log(input.textContent[input.textContent.length - 1]);
+                input.textContent = input.textContent.replace(input.textContent[input.textContent.length - 1], '')
+            } else {
+                input.textContent += targetBtn.textContent;
+        
+            }
+            console.log(targetBtn.textContent);
+            console.log(input.textContent);
+
+        }
+
+    })
+    */
+
+
+    console.log(correctAnswers);
+
+
+}
+
+
+function displayResults() {
+    let name = input.value;
+    console.log("name: ", name);
+    loginContainer.style.display = "none";
+    quizContainer.style.display = "none";
+    resultsContainer.style.display = "flex";
+    console.log("correctAnswers", correctAnswers);
+    console.log("questions length", questions.length);
+    let percentage = Math.round(correctAnswers / questions.length * 100);
+    console.log("fghfghfg");
+    console.log("percentage:", percentage);
+    let mark = 0
+    if (percentage > 80) {
+        mark = 5;
+    } else if (percentage < 81 && percentage > 62) {
+        mark = 4;
+    } else if (percentage < 63 && percentage > 49) {
+        mark = 3;
+    } else {
+        mark = 2;
+    }
+    let results__mark = document.querySelector(".results__mark");
+    let results__percentage = document.querySelector(".results__percentage");
+    results__mark.textContent = `${name}, ваша оценка ${mark}`;
+    results__percentage.textContent = `Правильных ответов ${correctAnswers} из ${questions.length} (${percentage}%)`;
+
+
+
+
+
+
+
+    /*
+    
+    
+    5	91-100%
+    5-	81-90 %
+    4	71-80 %
+    4-	63-70 %
+    3	56-62 %
+    3-	50-55 %
+    2	25-49 %
+    2-	0-24 %
+    
+    
+    */
+
+
+
+
+    /*
+
+    
+Иван, ваша оценка 3 
+правильных ответов 3 из 6 (50%)
+Вы ответили неверно на следующие вопросы:
+Какой язык программирования вы изучаете?
+Что такое CSS?
+
+    */
+
 
 
 
@@ -58,7 +237,10 @@ function displayQuestion() {
 
 }
 
+displayLogin();
+/*
 displayQuestion();
+*/
 
 
 
